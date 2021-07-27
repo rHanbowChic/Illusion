23c538f4f6be2851fb180cc8fa5177e951024020
asus <asus@DESKTOP-HG7MEKR>
2021-07-27 20:14:34 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace Illusion
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.lnkBox = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.OKButton = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.colorNote = new System.Windows.Forms.Label();
+            this.buttonChangeColor = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // lnkBox
+            // 
+            this.lnkBox.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lnkBox.FormattingEnabled = true;
+            this.lnkBox.ItemHeight = 17;
+            this.lnkBox.Location = new System.Drawing.Point(40, 25);
+            this.lnkBox.Name = "lnkBox";
+            this.lnkBox.Size = new System.Drawing.Size(321, 208);
+            this.lnkBox.TabIndex = 0;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(390, 25);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(106, 23);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "使用深色字体";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // OKButton
+            // 
+            this.OKButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OKButton.Location = new System.Drawing.Point(390, 208);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(233, 31);
+            this.OKButton.TabIndex = 3;
+            this.OKButton.Text = "应用磁贴";
+            this.OKButton.UseVisualStyleBackColor = true;
+            this.OKButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // colorNote
+            // 
+            this.colorNote.AutoSize = true;
+            this.colorNote.BackColor = System.Drawing.Color.Black;
+            this.colorNote.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.colorNote.ForeColor = System.Drawing.Color.White;
+            this.colorNote.Location = new System.Drawing.Point(454, 123);
+            this.colorNote.Name = "colorNote";
+            this.colorNote.Size = new System.Drawing.Size(31, 12);
+            this.colorNote.TabIndex = 5;
+            this.colorNote.Text = "0 0 0";
+            // 
+            // buttonChangeColor
+            // 
+            this.buttonChangeColor.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonChangeColor.Location = new System.Drawing.Point(523, 113);
+            this.buttonChangeColor.Name = "buttonChangeColor";
+            this.buttonChangeColor.Size = new System.Drawing.Size(63, 31);
+            this.buttonChangeColor.TabIndex = 6;
+            this.buttonChangeColor.Text = "更改";
+            this.buttonChangeColor.UseVisualStyleBackColor = true;
+            this.buttonChangeColor.Click += new System.EventHandler(this.buttonChangeColor_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(387, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 19);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "磁贴颜色";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.Location = new System.Drawing.Point(534, 26);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 23);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "显示字体";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 282);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonChangeColor);
+            this.Controls.Add(this.colorNote);
+            this.Controls.Add(this.OKButton);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.lnkBox);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Illusion v0.9";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lnkBox;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button OKButton;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label colorNote;
+        private System.Windows.Forms.Button buttonChangeColor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using IWshRuntimeLibrary;
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Illusion
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public Process p = new Process();
+        public string darkOrLight = "light";
+        public string showNameonoff = "on";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+            if (rd.Next(1, 80) == 1)
+            {
+                lnkBox.Font = new System.Drawing.Font("Wingdings", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));//An easter egg:3
+
+            }
+            DirectoryInfo TheFolder = new DirectoryInfo("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs");
+            ArrayList lnkList = new ArrayList();
+            foreach (FileInfo NextFile in TheFolder.GetFiles())
+                lnkList.Add(NextFile.Name);
+            foreach (DirectoryInfo NextFolder in TheFolder.GetDirectories())
+                foreach (FileInfo NextFile in NextFolder.GetFiles())
+                    lnkList.Add(NextFolder.Name + "\\" + NextFile.Name);
+            for (int i = lnkList.Count - 1; i >= 0; i--)//.lnk files
+            {
+                if (!Regex.IsMatch(lnkList[i].ToString(), @"(.*)(\.lnk)"))
+                {
+                    lnkList.RemoveAt(i);
+                }
+            }
+            for (int i = lnkList.Count - 1; i >= 0; i--)//remove .lnk
+            {
+                lnkList[i] = lnkList[i].ToString().Substring(0, lnkList[i].ToString().Length - 4);
+            }
+            for (int i = 0; i < lnkList.Count; i++)//To lnkBox
+            {
+                this.lnkBox.Items.Add(lnkList[i]);
+            }
+            lnkBox.SelectedIndex = 0;
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();//Start a cmd.exe profile for Python
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string lnkPath = "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\" + lnkBox.SelectedItem + ".lnk";
+            IWshRuntimeLibrary.WshShell shell = new IWshRuntimeLibrary.WshShellClass();
+            IWshRuntimeLibrary.IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(lnkPath);
+            string exePath = shortcut.TargetPath;
+            if (!(exePath.Contains("C:\\Windows") || exePath.Contains("C:\\windows")))
+            {
+                try
+                {
+                    Icon exeIcon = Icon.ExtractAssociatedIcon(exePath);
+                    Bitmap exeIconBmp = exeIcon.ToBitmap();
+                    exeIconBmp.Save("exeicon.png", System.Drawing.Imaging.ImageFormat.Png);
+                    p.StandardInput.WriteLine(".\\Python38-32\\python .\\imagep.py " + colorNote.Text);
+                    Thread.Sleep(600);//Wait for Python
+                    XmlDocument vManifest = new XmlDocument();
+                    vManifest.Load("Template.xml");
+                    XmlElement xe = (XmlElement)vManifest.SelectSingleNode("Application/VisualElements");
+                    xe.SetAttribute("ForegroundText", darkOrLight);
+                    xe.SetAttribute("ShowNameOnSquare150x150Logo", showNameonoff);
+                    exePath = exePath.Substring(0, exePath.Length - 4);
+                    vManifest.Save(exePath + ".VisualElementsManifest.xml");
+                    exePath = exePath.Substring(0, exePath.LastIndexOf("\\") + 1);
+                    p.StandardInput.WriteLine("xcopy /Y tile.png \"" + exePath + "\"");
+                    p.StandardInput.WriteLine("xcopy /Y tileSmall.png \"" + exePath + "\"");
+                    /*p.StandardInput.WriteLine("xcopy /Y \"" + lnkPath + "\" .\\");
+                    p.StandardInput.WriteLine("del \"" + lnkPath + "\"");
+                    Thread.Sleep(2000);//Wait for System
+                    string lnkName = lnkPath.Substring(lnkPath.LastIndexOf("\\") + 1, lnkPath.Length - lnkPath.LastIndexOf("\\") - 1);
+                    lnkPath = lnkPath.Substring(0, lnkPath.LastIndexOf("\\") + 1);
+                    p.StandardInput.WriteLine("xcopy /Y " + lnkName + " \"" + lnkPath + "\"");*/
+
+                    p.StandardInput.WriteLine(".\\clean");
+
+
+                }
+                catch
+                {
+                    MessageBox.Show("这个程序没有图标或无法提取.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("这是系统程序.");
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                darkOrLight = "dark";
+            }
+            else
+            {
+                darkOrLight = "light";
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                showNameonoff = "on";
+            }
+            else
+            {
+                showNameonoff = "off";
+            }
+        }
+
+        private void buttonChangeColor_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            Color colorChoosed = colorDialog1.Color;
+            colorNote.Text = colorChoosed.R.ToString() + " " + colorChoosed.G.ToString() + " " + colorChoosed.B.ToString();
+            colorNote.BackColor = System.Drawing.Color.FromArgb(colorChoosed.R,colorChoosed.G,colorChoosed.B);
+        }
+    }
+}
